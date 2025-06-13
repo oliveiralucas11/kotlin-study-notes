b1cf54795c6a9aa2f1a0462f9f88fa99e7248399
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2025-06-13 16:36:25 -0400

    inicial

--- Fundamentos/src/main/kotlin/fundamentos/Main.kt
+++ Fundamentos/src/main/kotlin/fundamentos/Main.kt
@@ -1,7 +1,13 @@
 package fundamentos
+// O Kotlin nao obriga a definir os pacotes, porem é uma boa pratica sempre ter definido o package de acordo com o pacote que vc está
+// Pacotes sempre sao definidos com letras minusculas
+
 
 fun main() {
+    // O programa kotlin comeca sempre com o metodo main
+    // Kotlin nao usa ; somente usamos quando colocamos dois comandos em uma mesma linha, pois o compilador nao sabe onde comeca e onde termina o comando
 
-    println("Hello")
+
+    println("Hello"); println("Hello2") // Exemplo usando ;
 
 }
--- Fundamentos/src/main/kotlin/fundamentos/VarVal.kt
+++ Fundamentos/src/main/kotlin/fundamentos/VarVal.kt
@@ -3,12 +3,16 @@ package fundamentos
 fun main() {
 
     // Em kotlin quando queremos criar uma variavel usamos a palavra chave var
-    var counter: Int  // Definimos explicitamente a tipagem da variavel, podemos atribuir o valor agora ou depois
-    counter = 10
+    var counter: Int = 10  // Definimos explicitamente a tipagem da variavel, podemos atribuir o valor agora ou depois
 
     var counter2: Int = 15 // Aqui estamos atribuindo o valor na mesma linha
 
     var counter3 = 10 // Dessa forma está sendo feita de forma implicita
+
+    var counter6: Double = 22.2
+
+    // Melhor que seja sempre declarado pela forma implicita , por causa do inference type.
+    // Usar tambem sempre val como declaracao padrao
 
 
 
