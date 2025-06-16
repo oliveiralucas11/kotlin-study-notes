24fa447194f4e70001ef7e59ffd5ca82bfa85c8b
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2025-06-15 23:15:19 -0400

    inicial

--- Fundamentos/src/main/kotlin/fundamentos/ConvertTypes.kt
+++ Fundamentos/src/main/kotlin/fundamentos/ConvertTypes.kt
@@ -1,5 +1,6 @@
 package fundamentos
 
+
 fun main() {
 
     val mediaDePreco: Int = 22
@@ -9,6 +10,12 @@ fun main() {
 
     // Convertendo int para string
     val resposta2 = mediaDePreco.toString()
+
+    // Convertendo int para long
+    val resposta3 = mediaDePreco.toLong()
+
+    // Convertendo int para float
+    val resposta4 = mediaDePreco.toFloat()
 
     println(resposta)
     println(resposta::class)
